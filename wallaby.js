f7f56567e6239502a4008710ccea45449cbfd235
handeman67 <steve1@who.net>
2021-11-25 21:03:24 -0500

    more added

--- wallaby.js
+++ wallaby.js
@@ -1,58 +1,54 @@
-module.exports = () => {
-  return {
- name: 'Centipeed',
-    files: [
-      
-      {
-        pattern: 'p5.min.js',
-        instrument: false,
-        
-      },
-      {
-        pattern: 'p5.dom.min.js',
-        instrument: false,
-      
-      },
-      {
-        pattern: 'p5.sound.min.js',
-        instrument: false,
-      
-      },
-{
-        pattern: 'ball.js',
-        instrument: false,
-       
-      },
-                
-    {
-        pattern: 'blocks.js',
-       
-      },
-      {
-        pattern: 'player.js',
-        instrument: false,
-       
-      },
-       {
-        pattern: 'sketch.js',
-        instrument: false,
-        
-      },
-     
-      
-    ],
-    tests: [
-      'js/test/*test.js',
-       
-    ],
-     
-    env: {
+module.exports = () => ({
+  autoDetect: true,
 
-      kind: 'chrome'
+  name: 'Breakout',
+  files: [
+    "index.html",
+    "/js/*.js",
+    "/css/*.css",
+    {
+      pattern: ['p5.min.js', 'p5.dom.min.js', 'p5.sound.min.js',
+        {
+          instrument: false,
+        }
+      ],
+
     },
-    workers: {
-      restart: true
-    },
-    debug: true
-  };
-}
+    // {
+    //         pattern: 'ball.js',
+    //         instrument: true,
+
+    //       },
+
+    //     {
+    //         pattern: 'blocks.js',
+
+    //       },
+    //       {
+    //         pattern: 'player.js',
+    //         instrument: true,
+
+    //       },
+    //        {
+    //         pattern: 'sketch.js',
+    //         instrument: true,
+
+    //       },
+
+
+  ],
+  tests: [
+    'js/test/*test.js',
+
+  ],
+
+  env: {
+
+    kind: 'chrome'
+  },
+  workers: {
+    restart: true
+  },
+  debug: true
+
+});
